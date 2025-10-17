1f999c35f0d546288d325b989dddec83e91b5724
CodeOne <davidhagon@googlemail.com>
2025-10-17 01:18:17 +0100

    first commit 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  integrations: [react(), tailwind()],
+  adapter: cloudflare()
 });
